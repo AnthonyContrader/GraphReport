7e3a53bcf0ed5e01ef6e2dfe90423c6de7b0d172
unknown <decrescenzotonio@gmail.com>
2020-03-20 18:11:51 +0100

    t

--- src/main/webapp/js/toniojs.js
+++ src/main/webapp/js/toniojs.js
@@ -10,10 +10,6 @@ function confDelDS(){
 	return confirm('\nCancellazione in corso!\n\nStai per eliminare definitivamente questo DataSet.\nVuoi proseguire?');
 }
 
-function confDelUt(){
-	return confirm('\nCancellazione in corso!\n\nStai per eliminare definitivamente questo account.\nVuoi proseguire?');
-}
-
 function checkColonna(id,conf){
 	var conf = conf || false;
 	if(!document.getElementById('btnSalva').disabled){
