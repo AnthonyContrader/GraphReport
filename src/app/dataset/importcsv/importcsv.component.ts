52695e2bc451713b7e94b02897e3ce3d9ab1fdec
pierolocriti <pierolocriti@gmail.com>
2020-04-02 18:27:43 +0200

    Piero

--- src/app/dataset/importcsv/importcsv.component.ts
+++ src/app/dataset/importcsv/importcsv.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataSetService } from 'src/service/DataSetService';
 
 @Component({
@@ -13,4 +13,29 @@ export class ImportcsvComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public csvRecords: any[] = [];
+  @ViewChild('fileImportInput') fileImportInput: any;
+    fileChangeListener($event: any): void {
+      var files = $event.srcElement.files;
+      if (files[0].name.endsWith('.csv')) {
+        var input = $event.target;
+        var reader = new FileReader();
+        reader.readAsText(input.files[0]);
+        reader.onload = (data) => {
+          let csvData = reader.result;
+          let csvRecordsArray = (csvData as string).split(/\r\n|\n/);
+          for(let i=0;i<csvRecordsArray.length;i++){
+          let rowdata = csvRecordsArray[i].match(/("[^"]*")|[^,]+/g);
+          this.csvRecords.push(rowdata);
+          }
+        };
+        reader.onerror = function() {
+          alert('Unable to read ' + input.files[0]);
+        };
+      } else {
+        alert('Please import valid .csv file.');
+        this.fileImportInput.nativeElement.value = '';
+        this.csvRecords = [];
+      }
+    }
 }
