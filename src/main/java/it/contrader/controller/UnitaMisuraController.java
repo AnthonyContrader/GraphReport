78d0e23f7107450070ac57245e7c1fe2433d877a
Riccardo <andria.riccardo@libero.it>
2020-04-02 12:28:33 +0200

    Riccardo-0204-1228

--- src/main/java/it/contrader/controller/UnitaMisuraController.java
+++ src/main/java/it/contrader/controller/UnitaMisuraController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,7 +56,7 @@ public class UnitaMisuraController extends AbstractController<UnitaMisuraDTO>{
 		return true;
     }
     
-    @PostMapping("/findAll")
+    @GetMapping("/findAll")
     public List<UnitaMisuraDTO> findAll(@RequestParam("cerca") String daCercare){
         return unitamisuraService.findAll(daCercare);
     }
