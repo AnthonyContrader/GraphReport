c9eb55a7e3e5d4bcf4ed45ea9d7a5050efe1904c
pierolocriti <pierolocriti@gmail.com>
2020-05-05 20:00:44 +0200

    dto e converter

---
+++ Presentazione/Presentazione/Converter/DiapographConverter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop.Infrastructure;
+using Presentazione.DTO;
+using Presentazione.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentazione.Converter
+{
+    public class DiapographConverter : AConverter<DiapographDTO, Diapograph>
+    {
+        public override DiapographDTO ToDTO(Diapograph model)
+        {
+            DiapographDTO dto = new DiapographDTO();
+            dto.id = model.id;
+            dto.dimensione = model.dimensione;
+            dto.posizione = model.posizione;
+            dto.diapositiva = new DiapositivaDTO();
+            dto.diapositiva.id = model.diapositiva.id;
+            dto.idGraph = model.idGraph;
+
+            return dto;
+        }
+
+        public override Diapograph ToModel(DiapographDTO dto)
+        {
+            Diapograph model = new Diapograph();
+            model.id = dto.id;
+            model.dimensione = dto.dimensione;
+            model.posizione = dto.posizione;
+            model.diapositiva = new Diapositiva();
+            model.diapositiva.id = dto.diapositiva.id;
+            model.idGraph = dto.idGraph;
+
+            return model;
+        }
+    }
+}
---
+++ Presentazione/Presentazione/Converter/TestoConverter.cs
@@ -0,0 +1,44 @@
+﻿using Presentazione.DTO;
+using Presentazione.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentazione.Converter
+{
+    public class TestoConverter : AConverter<TestoDTO, Testo>
+    {
+        public override TestoDTO ToDTO(Testo model)
+        {
+            TestoDTO dto = new TestoDTO();
+            dto.id = model.id;
+            dto.text = model.text;
+            dto.posizione = model.posizione;
+            dto.colore = model.colore;
+            dto.dimensione = model.dimensione;
+            dto.fontSize = model.fontSize;
+            dto.fontStyle = model.fontStyle;
+            dto.diapositiva = new DiapositivaDTO();
+            dto.diapositiva.id = model.diapositiva.id;
+
+            return dto;
+        }
+
+        public override Testo ToModel(TestoDTO dto)
+        {
+            Testo model = new Testo();
+            model.id = dto.id;
+            model.text = dto.text;
+            model.posizione = dto.posizione;
+            model.colore = dto.colore;
+            model.dimensione = dto.dimensione;
+            model.fontSize = dto.fontSize;
+            model.fontStyle = dto.fontStyle;
+            model.diapositiva = new Diapositiva();
+            model.diapositiva.id = dto.diapositiva.id;
+
+            return model;
+        }
+    }
+}
