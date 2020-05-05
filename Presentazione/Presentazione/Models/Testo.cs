80b068d64a9181d4157ed616b314cf4f4932b67d
pierolocriti <pierolocriti@gmail.com>
2020-05-05 16:04:41 +0200

    aaa

---
+++ Presentazione/Presentazione/Models/Testo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentazione.Models
+{
+    [Table("Testo")]
+    [Serializable]
+    public class Testo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        [Required]
+        public string text { get; set; }
+
+        [Required]
+        public string dimensione { get; set; }
+
+        [Required]
+        public string posizione { get; set; }
+
+        [Required]
+        public int fontSize { get; set; }
+
+        [Required]
+        public string colore { get; set; }
+
+        [Required]
+        public int fontStyle { get; set; }
+
+        [Required]
+        public Diapositiva diapositiva { get; set; }
+
+
+
+    }
+}
