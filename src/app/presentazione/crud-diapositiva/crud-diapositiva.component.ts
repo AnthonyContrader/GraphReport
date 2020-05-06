fe71618ed9821627407c57c9fc2a90e72ae447d5
andriariccardo <61690315+andriariccardo@users.noreply.github.com>
2020-05-06 12:44:14 +0200

    Riccardo-FrontEnd

--- src/app/presentazione/crud-diapositiva/crud-diapositiva.component.ts
+++ src/app/presentazione/crud-diapositiva/crud-diapositiva.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DiapositivaService } from 'src/service/diapositiva.service';
 
 @Component({
   selector: 'app-crud-diapositiva',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudDiapositivaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: DiapositivaService) { }
 
   ngOnInit(): void {
   }
