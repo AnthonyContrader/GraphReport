bf0b7b92bb937fb831bc7625d8438d8e3ff60319
pierolocriti <pierolocriti@gmail.com>
2020-05-28 18:08:59 +0200

    Piero

---
+++ Presentazione/Presentazione/Migrations/20200506184921_LaMigration.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Presentazione.Migrations
+{
+    public partial class LaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Presentazione",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(nullable: false),
+                    dataCreazione = table.Column<DateTime>(nullable: false),
+                    ultimaModifica = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Presentazione", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Diapositiva",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    presentazioneId = table.Column<long>(nullable: false),
+                    sfondo = table.Column<string>(nullable: false),
+                    ordine = table.Column<int>(nullable: false),
+                    preset = table.Column<bool>(nullable: false),
+                    ratio = table.Column<string>(nullable: false),
+                    isTitolo = table.Column<bool>(nullable: false),
+                    titolo = table.Column<string>(nullable: true),
+                    posizioneT = table.Column<string>(nullable: true),
+                    dimensioneT = table.Column<string>(nullable: true),
+                    coloreT = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Diapositiva", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Diapositiva_Presentazione_presentazioneId",
+                        column: x => x.presentazioneId,
+                        principalTable: "Presentazione",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiapoGraph",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    dimensione = table.Column<string>(nullable: false),
+                    posizione = table.Column<string>(nullable: false),
+                    diapositivaid = table.Column<long>(nullable: false),
+                    idGraph = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiapoGraph", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_DiapoGraph_Diapositiva_diapositivaid",
+                        column: x => x.diapositivaid,
+                        principalTable: "Diapositiva",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Testo",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    text = table.Column<string>(nullable: false),
+                    dimensione = table.Column<string>(nullable: false),
+                    posizione = table.Column<string>(nullable: false),
+                    fontSize = table.Column<int>(nullable: false),
+                    colore = table.Column<string>(nullable: false),
+                    fontStyle = table.Column<int>(nullable: false),
+                    diapositivaid = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testo", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Testo_Diapositiva_diapositivaid",
+                        column: x => x.diapositivaid,
+                        principalTable: "Diapositiva",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiapoGraph_diapositivaid",
+                table: "DiapoGraph",
+                column: "diapositivaid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Diapositiva_presentazioneId",
+                table: "Diapositiva",
+                column: "presentazioneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Testo_diapositivaid",
+                table: "Testo",
+                column: "diapositivaid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiapoGraph");
+
+            migrationBuilder.DropTable(
+                name: "Testo");
+
+            migrationBuilder.DropTable(
+                name: "Diapositiva");
+
+            migrationBuilder.DropTable(
+                name: "Presentazione");
+        }
+    }
+}
---
+++ Presentazione/Presentazione/Migrations/20200518174531_migrazione.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Presentazione.Migrations
+{
+    public partial class migrazione : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Diapositiva_Presentazione_presentazioneId",
+                table: "Diapositiva");
+
+            migrationBuilder.RenameColumn(
+                name: "presentazioneId",
+                table: "Diapositiva",
+                newName: "presentazioneid");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Diapositiva_presentazioneId",
+                table: "Diapositiva",
+                newName: "IX_Diapositiva_presentazioneid");
+
+            migrationBuilder.AddColumn<long>(
+                name: "utente",
+                table: "Presentazione",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "dimensioneT",
+                table: "Diapositiva",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "borderColor",
+                table: "Diapositiva",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "borderSize",
+                table: "Diapositiva",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "fontFamily",
+                table: "Diapositiva",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "fontRotation",
+                table: "Diapositiva",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "fontStyle",
+                table: "Diapositiva",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Diapositiva_Presentazione_presentazioneid",
+                table: "Diapositiva",
+                column: "presentazioneid",
+                principalTable: "Presentazione",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Diapositiva_Presentazione_presentazioneid",
+                table: "Diapositiva");
+
+            migrationBuilder.DropColumn(
+                name: "utente",
+                table: "Presentazione");
+
+            migrationBuilder.DropColumn(
+                name: "borderColor",
+                table: "Diapositiva");
+
+            migrationBuilder.DropColumn(
+                name: "borderSize",
+                table: "Diapositiva");
+
+            migrationBuilder.DropColumn(
+                name: "fontFamily",
+                table: "Diapositiva");
+
+            migrationBuilder.DropColumn(
+                name: "fontRotation",
+                table: "Diapositiva");
+
+            migrationBuilder.DropColumn(
+                name: "fontStyle",
+                table: "Diapositiva");
+
+            migrationBuilder.RenameColumn(
+                name: "presentazioneid",
+                table: "Diapositiva",
+                newName: "presentazioneId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Diapositiva_presentazioneid",
+                table: "Diapositiva",
+                newName: "IX_Diapositiva_presentazioneId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "dimensioneT",
+                table: "Diapositiva",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Diapositiva_Presentazione_presentazioneId",
+                table: "Diapositiva",
+                column: "presentazioneId",
+                principalTable: "Presentazione",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
